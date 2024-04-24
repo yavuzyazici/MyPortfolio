344f8d019eacfc5d21221e5862db90f32c9ae854
Yavuz Yazici <4yavuzyazici@gmail.com>
2024-04-24 17:49:15 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Yavuz_Selim_Yazıcı.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
