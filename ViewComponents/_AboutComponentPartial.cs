01ba7bc6eac567469283aba4dbc51ae7bee8eb54
Yavuz Selim YAZICI <yavuzselimyazici41@gmail.com>
2024-04-25 18:23:44 +0300

    New Components added

---
+++ ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yavuz_Selim_Yazıcı.ViewComponents
+{
+    public class _AboutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewComponents/_SkillComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yavuz_Selim_Yazıcı.ViewComponents
+{
+    public class _SkillComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
